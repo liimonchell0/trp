eeed06c48b466bdfea661e4b7ec139e9f61c83b6
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2021-12-18 11:17:12 +0300

    feat: new files

---
+++ ConsoleApplication108.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "RU");
+	cout << "Ввести координаты точки B(x, y) и радиус окружности R." << endl<<
+	"Центром окружности является начало координат.Определить лежит ли" << endl<<
+	"данная точка внутри окружности."<<endl;
+	double x, y, R;
+	cout << "x="; cin >> x;
+	cout << "y="; cin >> y;
+	cout << "R="; cin >> R;
+	if ((x*x + y * y) < R*R)
+		cout << "Лежит" << endl;
+	else cout << "Не лежит" << endl;
+	return 0;
+
+
+
+}
